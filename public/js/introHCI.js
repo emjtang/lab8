d5b4b0446a5955a88176a06f38cc2b4374b23a88
vagrant <vagrant@precise32.(none)>
2014-02-26 23:10:20 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(sendAnalysis);
+
+}
+function sendAnalysis(e){
+	ga("send", "event", "like", "click");
 }
